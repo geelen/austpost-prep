878dd8ecfb9ce0c1bb67449f691340c7a5461507
Glen Maddern <glenmaddern@gmail.com>
2019-09-24 10:27:10 +1000

    change ui

--- src/App.js
+++ src/App.js
@@ -13,16 +13,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {env.FRAMEWORK_NAME} is the best!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn {env.FRAMEWORK_NAME}
-        </a>
       </header>
     </div>
   );
